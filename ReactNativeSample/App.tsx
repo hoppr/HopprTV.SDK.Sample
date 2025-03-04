54c1ad6eb800649d9edf227153a5960b0d0f3bfa
Maxhoppr <max.bourdin@hoppr.com.au>
2025-03-04 17:30:38 +0800

    wip

--- ReactNativeSample/App.tsx
+++ ReactNativeSample/App.tsx
@@ -8,7 +8,8 @@ import { ScreenTwo } from './ScreenTwo';
 import { ScreenThree } from './ScreenThree';
 import { ScreenVideo } from './ScreenVideo';
 import Hoppr from 'react-native-hoppr';
-import { Config } from './Config';
+// import { Config } from './Config';
+const APP_KEY = process.env.HOPPR_APP_KEY || "DEFAULT_APP_KEY";
 
 export default function App() {
   const [result, setResult] = useState<number | undefined>();
@@ -16,7 +17,7 @@ export default function App() {
   const Stack = createNativeStackNavigator();
 
   useEffect(() => {
-    Hoppr.init(Config.APP_KEY, "ReactNativeSdkTestUser", {
+    Hoppr.init(APP_KEY, "ReactNativeSdkTestUser", {
       userLevel: "Beginner",
       age: 36,
       premium: true,
