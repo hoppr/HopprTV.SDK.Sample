8110c31056fdec16be55214c086d4f80e086fe3d
Maxhoppr <max.bourdin@hoppr.com.au>
2025-03-07 15:13:20 +0800

    w

--- JetStreamCompose/jetstream/src/main/java/com/google/jetstream/JetStreamApplication.kt
+++ JetStreamCompose/jetstream/src/main/java/com/google/jetstream/JetStreamApplication.kt
@@ -18,6 +18,7 @@ package com.google.jetstream
 
 import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import androidx.core.os.bundleOf
 import com.google.jetstream.data.repositories.MovieRepository
 import com.google.jetstream.data.repositories.MovieRepositoryImpl
@@ -34,8 +35,7 @@ class JetStreamApplication : Application(){
     override fun onCreate() {
         super.onCreate()
 
-//        val dev = "URg1C7IDyYAscqXirvmjp-Ax87ld5syOtxV6BopZYF6TSaE_7-CJg8VU8HWl7TIZzlzOgTgLYsOIkzZ-vZ7-gQ"
-
+        Log.d("maxdebug", "BuildConfig.APP_KEY ${BuildConfig.APP_KEY}")
         Hoppr.init(
             this,
             userId = UUID.randomUUID().toString(),
